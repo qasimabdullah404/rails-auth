1129c193d83e2975ada866028d62852dbaec7dde
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-11-01 02:41:18 +0500

    add some devise customization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :config_permitted_params, if: :devise_controller?
+    protected
+    def config_permitted_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :role
+  belongs_to :role, optional: true
 
   has_many :itens, dependent: :destroy
+
+  validates :name, presence: true
+  before_save :assign_role
+
+  def assign_role
+    self.role = Role.find_by name: 'Regular' if role.nil?
+  end
 end
