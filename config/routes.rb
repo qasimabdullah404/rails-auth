a25e728f559602fb77cd9cdb4ef837048759fbeb
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-11-01 02:29:35 +0500

    devised

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  scope '/admin' do
+    resources :users
+  end
+
+  root to: 'items#index'
+
   resources :items
   resources :users
   resources :roles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
